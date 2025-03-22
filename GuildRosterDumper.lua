1758c8e7ffc173ccfcbf6b722076e6acebf46e54
droopystaff <droopystaff.wow@gmail.com>
2025-03-22 02:17:42 -0400

    Tidied up a little

--- GuildRosterDumper.lua
+++ GuildRosterDumper.lua
@@ -1,44 +1,41 @@
 function GuildRosterDumper_OnLoad()
 		SLASH_GUILDDUMP1 = "/grd"
 		SLASH_GUILDDUMP2 = "/grdump"
-		SlashCmdList["GUILDDUMP"] = GuildRosterDumper_exportData
+		SlashCmdList["GUILDROSTERDUMP"] = GuildRosterDumper_exportData
 end
 
 function GuildRosterDumper_exportData()
-		GuildRosterDumperData = {}
-		GuildRosterDumperDataDB = {}
+		GuildRosterData = {}
 		
 		for i = 1, GetNumGuildMembers(true) do
-				DEFAULT_CHAT_FRAME:AddMessage("Attempting RosterNum" .. i .. "\r\n")
-		
-				local name, rank, rankIndex, level, class, zone, note, officerNote, _ = GetGuildRosterInfo(i)
-
+				local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11 = GetGuildRosterInfo(i)
+				
 				GuildRosterDumperDataDB[i] = {}
-				GuildRosterDumperDataDB[i].name = name
-				GuildRosterDumperDataDB[i].rank = rank
-				GuildRosterDumperDataDB[i].rankIndex = rankIndex
-				GuildRosterDumperDataDB[i].level = level
-				GuildRosterDumperDataDB[i].class = class
-				GuildRosterDumperDataDB[i].zone = zone
-				GuildRosterDumperDataDB[i].note = note
-				GuildRosterDumperDataDB[i].officerNote = officerNote
-
+				GuildRosterDumperDataDB[i].arg1 = arg1
+				GuildRosterDumperDataDB[i].arg2 = arg2
+				GuildRosterDumperDataDB[i].arg3 = arg3
+				GuildRosterDumperDataDB[i].arg4 = arg4
+				GuildRosterDumperDataDB[i].arg5 = arg5
+				GuildRosterDumperDataDB[i].arg6 = arg6
+				GuildRosterDumperDataDB[i].arg7 = arg7
+				GuildRosterDumperDataDB[i].arg8 = arg8
+				GuildRosterDumperDataDB[i].arg9 = arg9
+				GuildRosterDumperDataDB[i].arg10 = arg10
+				GuildRosterDumperDataDB[i].arg11 = arg11
 		end
 				
 		guild = GetGuildInfo("player")
-		GuildRosterDumperDataFrame = "Guild Roster Dump for " .. guild .. " at " .. date("%Y-%m-%d %H:%M:%S") .. "\r" .. "\r" .. "\r" .. "\r" .. "name,rank,level,class,zone,note,officerNote" .. "\r"  .. "\r" .. "\r"
+		GuildRosterDumperDataFrame = "Guild Roster Dump for " .. guild .. " at " .. date("%y-%m-%d %H:%M:%S") .. "\r\n" .. "\r\n"
 		
-		for i in GuildRosterDumperDataDB do
-				GuildRosterDumperDataFrame = GuildRosterDumperDataFrame .. GuildRosterDumperDataDB[i].name .. "," .. GuildRosterDumperDataDB[i].rank .. "," .. GuildRosterDumperDataDB[i].level .. "," .. GuildRosterDumperDataDB[i].class .. "," .. GuildRosterDumperDataDB[i].zone .. "," .. GuildRosterDumperDataDB[i].note .. "," .. GuildRosterDumperDataDB[i].officerNote .. "\r"
+		for i in GuildRosterDumperDB do
+				GuildRosterDumperFrame = GuildRosterDumperDataFrame .. GuildRosterDumperDataDB[i].arg1 .. "," .. GuildRosterDumperDataDB[i].arg2 .. "," .. GuildRosterDumperDataDB[i].arg3 .. "," .. GuildRosterDumperDataDB[i].arg4 .. "," .. GuildRosterDumperDataDB[i].arg5 .. "," .. GuildRosterDumperDataDB[i].arg6 .. "," .. GuildRosterDumperDataDB[i].arg7 .. "," .. GuildRosterDumperDataDB[i].arg8 .. "," .. GuildRosterDumperDataDB[i].arg9 .. "," .. GuildRosterDumperDataDB[i].arg10 .. "," .. GuildRosterDumperDataDB[i].arg11 .. "\r\n"
 		end
-		
-		GuildRosterDumperData = GuildRosterDumperDataFrame
 		
 		GuildRosterDumper_ExportRaidFrameEditBox1:SetFont("Fonts\\FRIZQT__.TTF", "8")
 		GuildRosterDumper_ExportRaidFrameEditBox1Left:Hide()
 		GuildRosterDumper_ExportRaidFrameEditBox1Middle:Hide()
 		GuildRosterDumper_ExportRaidFrameEditBox1Right:Hide()
-		GuildRosterDumper_ExportRaidFrameEditBox1:SetText(GuildRosterDumperData)
+		GuildRosterDumper_ExportRaidFrameEditBox1:SetText(GuildRosterDumperDataFrame)
 		
 		ShowUIPanel(GuildRosterDumper_ExportRaidFrame, 1)
 	
