0a1ee1e19b1deb06329cae5cfb68058d19f5bcda
droopystaff <droopystaff.wow@gmail.com>
2025-03-23 20:29:46 -0400

    Added time since last seen functionality

--- GuildRosterDumper.lua
+++ GuildRosterDumper.lua
@@ -1,41 +1,79 @@
 function GuildRosterDumper_OnLoad()
 		SLASH_GUILDDUMP1 = "/grd"
 		SLASH_GUILDDUMP2 = "/grdump"
-		SlashCmdList["GUILDROSTERDUMP"] = GuildRosterDumper_exportData
+		SlashCmdList["GUILDDUMP"] = GuildRosterDumper_exportData
+end
+
+function isEmpty(s)
+	return s == nil or s == ''
 end
 
 function GuildRosterDumper_exportData()
-		GuildRosterData = {}
+		GuildRosterDumperData = {}
+		GuildRosterDumperDataDB = {}
 		
 		for i = 1, GetNumGuildMembers(true) do
-				local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11 = GetGuildRosterInfo(i)
+				local name, rank, _, level, class, _, note, officerNote, _ = GetGuildRosterInfo(i)
+				local lastYear, lastMonth, lastDay, lastHour = GetGuildRosterLastOnline(i)
 				
+				DEFAULT_CHAT_FRAME:AddMessage("Attempting RosterNum" .. i .. " - " .. name .. "\r\n")
+
+				if string.find(note, ",") then
+					note = string.gsub(note, "%,", "%/")
+				end
+				
+				if isEmpty(note) then
+					note = " "
+				end
+				
+				if isEmpty(officerNote) then
+					officerNote = " "
+				end
+
+				if isEmpty(lastYear) then
+					lastYear = 0
+				end
+				
+				if isEmpty(lastMonth) then
+					lastMonth = 0
+				end
+				
+				if isEmpty(lastDay) then
+					lastDay = 0
+				end
+				
+				if isEmpty(lastHour) then
+					lastHour = 0
+				end
+
 				GuildRosterDumperDataDB[i] = {}
-				GuildRosterDumperDataDB[i].arg1 = arg1
-				GuildRosterDumperDataDB[i].arg2 = arg2
-				GuildRosterDumperDataDB[i].arg3 = arg3
-				GuildRosterDumperDataDB[i].arg4 = arg4
-				GuildRosterDumperDataDB[i].arg5 = arg5
-				GuildRosterDumperDataDB[i].arg6 = arg6
-				GuildRosterDumperDataDB[i].arg7 = arg7
-				GuildRosterDumperDataDB[i].arg8 = arg8
-				GuildRosterDumperDataDB[i].arg9 = arg9
-				GuildRosterDumperDataDB[i].arg10 = arg10
-				GuildRosterDumperDataDB[i].arg11 = arg11
+				GuildRosterDumperDataDB[i].name = name
+				GuildRosterDumperDataDB[i].rank = rank
+				GuildRosterDumperDataDB[i].level = level
+				GuildRosterDumperDataDB[i].class = class
+				GuildRosterDumperDataDB[i].note = note
+				GuildRosterDumperDataDB[i].officerNote = officerNote
+				GuildRosterDumperDataDB[i].lastYear = lastYear
+				GuildRosterDumperDataDB[i].lastMonth = lastMonth
+				GuildRosterDumperDataDB[i].lastDay = lastDay
+				GuildRosterDumperDataDB[i].lastHour = lastHour
+
 		end
 				
 		guild = GetGuildInfo("player")
-		GuildRosterDumperDataFrame = "Guild Roster Dump for " .. guild .. " at " .. date("%y-%m-%d %H:%M:%S") .. "\r\n" .. "\r\n"
+		GuildRosterDumperDataFrame = "Guild Roster Dump for " .. guild .. " at " .. date("%Y-%m-%d %H:%M:%S") .. "\r" .. "\r" .. "\r" .. "\r" .. "name,rank,level,class,note,officerNote,yearsOffline,monthsOffline,daysOffline,hoursOffline" .. "\r"  .. "\r" .. "\r"
 		
-		for i in GuildRosterDumperDB do
-				GuildRosterDumperFrame = GuildRosterDumperDataFrame .. GuildRosterDumperDataDB[i].arg1 .. "," .. GuildRosterDumperDataDB[i].arg2 .. "," .. GuildRosterDumperDataDB[i].arg3 .. "," .. GuildRosterDumperDataDB[i].arg4 .. "," .. GuildRosterDumperDataDB[i].arg5 .. "," .. GuildRosterDumperDataDB[i].arg6 .. "," .. GuildRosterDumperDataDB[i].arg7 .. "," .. GuildRosterDumperDataDB[i].arg8 .. "," .. GuildRosterDumperDataDB[i].arg9 .. "," .. GuildRosterDumperDataDB[i].arg10 .. "," .. GuildRosterDumperDataDB[i].arg11 .. "\r\n"
+		for i in GuildRosterDumperDataDB do
+				GuildRosterDumperDataFrame = GuildRosterDumperDataFrame .. GuildRosterDumperDataDB[i].name .. "," .. GuildRosterDumperDataDB[i].rank .. "," .. GuildRosterDumperDataDB[i].level .. "," .. GuildRosterDumperDataDB[i].class .. "," .. GuildRosterDumperDataDB[i].note .. "," .. GuildRosterDumperDataDB[i].officerNote .. "," .. GuildRosterDumperDataDB[i].lastYear .. "," .. GuildRosterDumperDataDB[i].lastMonth .. "," .. GuildRosterDumperDataDB[i].lastDay .. "," .. GuildRosterDumperDataDB[i].lastHour .. "\r"
 		end
+		
+		GuildRosterDumperData = GuildRosterDumperDataFrame
 		
 		GuildRosterDumper_ExportRaidFrameEditBox1:SetFont("Fonts\\FRIZQT__.TTF", "8")
 		GuildRosterDumper_ExportRaidFrameEditBox1Left:Hide()
 		GuildRosterDumper_ExportRaidFrameEditBox1Middle:Hide()
 		GuildRosterDumper_ExportRaidFrameEditBox1Right:Hide()
-		GuildRosterDumper_ExportRaidFrameEditBox1:SetText(GuildRosterDumperDataFrame)
+		GuildRosterDumper_ExportRaidFrameEditBox1:SetText(GuildRosterDumperData)
 		
 		ShowUIPanel(GuildRosterDumper_ExportRaidFrame, 1)
 	
